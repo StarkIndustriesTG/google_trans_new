e522581653f23a8a58496aa0bc0fa419f444f4fb
Danish <danish@ultroid.tech>
2021-09-15 22:49:02 +0530

    conflicts

--- setup.py
+++ setup.py
@@ -1,24 +1,18 @@
 import setuptools
-from google_trans_new import __version__
 
 with open("README.md", "r", encoding='utf-8') as f:
     long_desc = f.read()
 
-
-def _requires_from_file(filename):
-    return open(filename, encoding="utf8").read().splitlines()
-
-
 setuptools.setup(
     name="google-trans-new",
-    version=__version__,
+    version="0.2",
     author="lushan88a",
     description="google_trans_new",
     long_description=long_desc,
     long_description_content_type='text/markdown',
     url="https://github.com/lushan88a/google_trans_new",
     packages=setuptools.find_packages(),
-    install_requires=_requires_from_file('requirements.txt'),
+    install_requires=["urllib3", "requests"],
     classifiers=[
         "Programming Language :: Python :: 3.8",
         "License :: OSI Approved :: MIT License",
